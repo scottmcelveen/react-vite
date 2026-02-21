484d4fbc4c614028cc8d397b46d8d4095c34bbd8
Scott McElveen <scott.mcelveen@hotmail.com>
2026-02-21 16:45:19 +0000

    add tanstack query

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,10 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
 import { Link, Outlet } from "react-router";
+import { getUsers, type User } from "../../lib/api/typicode/users";
 
 function Welcome() {
   console.log('---- in the Welcome() component ----');
+
+  const { data } = useQuery({ queryKey: ['users'], staleTime: 5000, queryFn: getUsers })
   return (
     <>
       <h1>Welcome Component!</h1>
+      <ul>
+        {data?.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
       <Link to="/welcome/lazy">
         Lazy Loaded Component
       </Link>
